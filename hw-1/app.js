df9813cc0daddbc200786f82fd61feaad1bbbc24
Benton Rochester <brochester@cpbgroup.com>
2015-02-26 13:39:27 -0700

    undoing changes

--- hw-1/app.js
+++ hw-1/app.js
@@ -2,34 +2,19 @@ $(document).ready(function(){
 
   console.log("ready?");
   // Question 1: Add a class "active" to the element with "Cats" using the elements class name
-<<<<<<< HEAD
   // Write your solution here
-  $('.Cats').addClass("active");
 
 
   // Question 2: Add a class "active" to the element with "Walrus" using the elements ID name
   // Write your solution here
-  $('#Walrus').addClass("active");
+
 
   // Question 3: Add a class "active" to the element with "Camel" using jquery's eq() method
   // Write your solution here
-   $('.list-group-item').eq( 4 ).addClass("active");
+
 
   // Question 4: On a "click" event to the html element ".item", add class "flip".
   // Write your solution here
-  $('.item').click(function(event){
-    $(this).addClass('flip');   
-  });
-
-  $('.questions').hover(
-      function(){
-        $(this).animate({"font-size":"72px"},"fast");
-    },
-     function() {
-        $(this).animate({"font-size": "16px"}, "fast");
-   });
-=======
-
 
 
   // Question 2: Add a class "active" to the element with "Walrus" using the elements ID name
@@ -37,30 +22,9 @@ $(document).ready(function(){
 
 
   // Question 3: Add a class "active" to the element with "Camel" using jquery's eq() method
-
->>>>>>> parent of dd9fc36... adding comment
-
-
-  $(".list-group-item").mouseover(
-   function() {
-      $(this).animate({"marginTop": "76px"}, "700");
-  });    
- 
-   $(".list-group-item").mouseleave(
-   function() {
-      $(this).animate({"marginTop": "0px"}, "700");
-   });
-
-
 
 
   // Question 5 (Bonus): Use jquery's animate() method to do something.
   // Write your solution here
-  $(".flipper").click(function(event){
-    $(".front").animate({
-      opacity:0.8,
-      height: 'toggle'
-    }, 500, function(){
-    });
-  });
+
 });
