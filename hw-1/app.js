f227cf3e95244a386d78d33ee4c5f5fb1e229b36
Benton Rochester <brochester@cpbgroup.com>
2015-02-25 19:44:24 -0700

    adding first homework

---
+++ hw-1/app.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+  console.log("ready?");
+
+  // Question 1: Add a class "active" to the element with "Cats" using the elements class name
+
+
+
+  // Question 2: Add a class "active" to the element with "Walrus" using the elements ID name
+
+
+
+  // Question 3: Add a class "active" to the element with "Camel" using jquery's eq() method
+
+
+
+
+
+});
