41fbf7a9cff429a638981496ace290927a6f2279
Danny Tran <danny@dqtran.com>
2015-03-02 23:49:16 -0700

    It is working!

--- hw-1/app.js
+++ hw-1/app.js
@@ -4,27 +4,24 @@ $(document).ready(function(){
   // Question 1: Add a class "active" to the element with "Cats" using the elements class name
   // Write your solution here
 
+  $( ".Cats" ).addClass( "active" );
 
   // Question 2: Add a class "active" to the element with "Walrus" using the elements ID name
   // Write your solution here
+  $( "#Walrus").addClass( "active");
 
 
   // Question 3: Add a class "active" to the element with "Camel" using jquery's eq() method
   // Write your solution here
-
-
+  $(".list-group li").eq(4).addClass("active");
   // Question 4: On a "click" event to the html element ".item", add class "flip".
   // Write your solution here
-
-
-  // Question 2: Add a class "active" to the element with "Walrus" using the elements ID name
-
-
-
-  // Question 3: Add a class "active" to the element with "Camel" using jquery's eq() method
-
-
+  $(".item").click(function(){
+    $(".item").addClass("flip").animate({opacity: 0}, 1000,function ()
+    {
+        $('.item').animate({opacity:1}, 1000);
+    })
+  });
   // Question 5 (Bonus): Use jquery's animate() method to do something.
   // Write your solution here
-
 });
