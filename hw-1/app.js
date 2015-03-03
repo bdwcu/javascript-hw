4931ea30aef2451ccdb99cd65b2a0f4f9f5af252
Joe Toscano <realjoet9@gmail.com>
2015-03-03 14:50:44 -0700

    Joe finished his hw

--- hw-1/app.js
+++ hw-1/app.js
@@ -3,28 +3,27 @@ $(document).ready(function(){
   console.log("ready?");
   // Question 1: Add a class "active" to the element with "Cats" using the elements class name
   // Write your solution here
+  $('.Cats').addClass('active');
 
 
   // Question 2: Add a class "active" to the element with "Walrus" using the elements ID name
   // Write your solution here
+  $('#Walrus').addClass('active');
 
 
   // Question 3: Add a class "active" to the element with "Camel" using jquery's eq() method
   // Write your solution here
+  $('.list-group-item').eq(4).addClass('active');
 
 
   // Question 4: On a "click" event to the html element ".item", add class "flip".
   // Write your solution here
-
-
-  // Question 2: Add a class "active" to the element with "Walrus" using the elements ID name
-
-
-
-  // Question 3: Add a class "active" to the element with "Camel" using jquery's eq() method
-
+  $('.item').click(function(){
+      $(this).addClass('flip').animate({"width": "800px"})
+  })
 
   // Question 5 (Bonus): Use jquery's animate() method to do something.
   // Write your solution here
+
 
 });
