81cf152642f3f6db01d8a5fa1b57ef3847b786aa
Benton Rochester <brochester@cpbgroup.com>
2015-03-03 17:59:45 -0700

    deleted ton of stuff

--- hw-1/app.js
+++ hw-1/app.js
@@ -1,30 +1,5 @@
 $(document).ready(function(){
 
-  console.log("ready?");
-  // Question 1: Add a class "active" to the element with "Cats" using the elements class name
-  // Write your solution here
 
-
-  // Question 2: Add a class "active" to the element with "Walrus" using the elements ID name
-  // Write your solution here
-
-
-  // Question 3: Add a class "active" to the element with "Camel" using jquery's eq() method
-  // Write your solution here
-
-
-  // Question 4: On a "click" event to the html element ".item", add class "flip".
-  // Write your solution here
-
-
-  // Question 2: Add a class "active" to the element with "Walrus" using the elements ID name
-
-
-
-  // Question 3: Add a class "active" to the element with "Camel" using jquery's eq() method
-
-
-  // Question 5 (Bonus): Use jquery's animate() method to do something.
-  // Write your solution here
 
 });
